f7e9bde47f40f85a61aeb0bccbe0596c8272529a
kayyreem <katlehosibz@gmail.com>
2023-02-23 02:36:29 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- * main - Starting point 
+/*
+ * main - Starting point
  *
- * Return: 0 (Exit point)
+ * Return: 0 Exit point
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual uzzle");
 	return (0);
 }

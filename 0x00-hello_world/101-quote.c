f257c51a1d300631aed4c781634c14999ab1837d
kayyreem <katlehosibz@gmail.com>
2023-02-28 07:06:02 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - Printing the function
+ *
+ * Return : Return value
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
